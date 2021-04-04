81102714de1fd78fcd5e67cc6b470434d3193c2c
Manan Singh <singhmanan1995@gmail.com>
2021-04-04 20:02:20 +0530

    Add files via upload

--- employee.js
+++ employee.js
@@ -1,37 +1,9 @@
-const express =require('express');
-const router =express.Router();
-const multer =require('multer');
-const EmployeesController =require('../controllers/employees');
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-      cb(null, Date.now()+file.originalname.replace(/\s+/g, '-'));
-    }
-  });
-  const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
-  const upload = multer({
-    storage: storage,
-    limits: {
-      fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-  });
-router.get("/", EmployeesController.employees_get_all);
-
-router.post("/", EmployeesController.employees_create_employee);
-
-router.get("/:employeeId", EmployeesController.employees_get_employee);
-
-router.patch("/:employeeId",  EmployeesController.employees_update_employee);
-
-router.delete("/:employeeId", EmployeesController.employees_delete);
-
-module.exports=Router;
+const mongoose =require('mongoose');
+const employeeSchema = mongoose.Schema({
+_id:mongoose.Schema.Types.ObjectId,
+employee_name: {type:String ,required:true},
+employee_salary:{type:Number ,required:true},
+employee_age:{type:Number, required:true},
+profile_image:{type:String,required:true}
+});
+module.exports =mongoose.model('Employee',employeeSchema);
